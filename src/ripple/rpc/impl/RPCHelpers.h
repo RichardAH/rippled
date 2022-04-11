ef31af536ad34b730d81808ff0dc169991a11d65
Richard Holland <richard.holland@starstone.co.nz>
2022-04-11 22:55:24 +0000

    Add book_changes rpc

--- src/ripple/rpc/impl/RPCHelpers.h
+++ src/ripple/rpc/impl/RPCHelpers.h
@@ -30,6 +30,7 @@
 #include <ripple/rpc/Status.h>
 #include <ripple/rpc/impl/Tuning.h>
 #include <optional>
+#include <variant>
 #include <org/xrpl/rpc/v1/xrp_ledger.pb.h>
 
 namespace Json {
@@ -286,6 +287,14 @@ chooseLedgerEntryType(Json::Value const& params);
  */
 unsigned int
 getAPIVersionNumber(const Json::Value& value, bool betaEnabled);
+
+
+/** Return a ledger based on ledger_hash or ledger_index,
+    or an RPC error */
+std::variant<
+    std::shared_ptr<Ledger const>,
+    Json::Value>
+getLedgerByContext(RPC::JsonContext& context);
 
 }  // namespace RPC
 }  // namespace ripple
